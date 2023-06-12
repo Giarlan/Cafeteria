91b180468bc84d8d7bd7a28635b939a9bd2af883
Giarlan Santana <giarlansilva@yahoo.com>
2023-06-12 20:11:41 -0300

    CREATE THE ORDER BUTTON AND IMAGE CAROUSEL
       *Build - The order button has been created and styled
       *Build - An image carousel has been created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,3 +18,20 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      loop:true,
+
+    });
