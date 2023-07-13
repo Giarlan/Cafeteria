2fc85d730b548d859edfab8d8f5f3f447b70985b
Giarlan Santana <giarlansilva@yahoo.com>
2023-07-12 22:36:26 -0300

    ITERATE BETWEEN NAVBAR AND EACH SECTION
       *Build - The function that makes the interaction between the NavBar links and each section has been created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,27 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+
+      if(top => offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      }
+
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -24,7 +42,7 @@ var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 7500,
     disableOnInteraction: false
   },
   pagination: {
@@ -38,7 +56,7 @@ var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 7500,
     disableOnInteraction: false
   },
   loop:true,
